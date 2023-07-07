9b34a2abe5c966a6697534f7afd38cde417ddcb8
Ben Elpidius <Ben.Elpidius@twisthink.com>
2023-07-07 15:06:02 -0400

    test

---
+++ cupcake-compose/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,25 @@
+package com.example.cupcake.test
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.ComposeNavigator
+import org.junit.Before
+
+class CupcakeScreenNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
+        }
+    }
+}
